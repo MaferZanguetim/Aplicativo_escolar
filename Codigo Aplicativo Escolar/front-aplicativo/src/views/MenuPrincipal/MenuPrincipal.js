28d47fc4c8a5b794347974f7b71b2f191fe96943
Lucas Cabrera <lucas.cabrera@fatec.sp.gov.br>
2023-05-29 05:34:45 -0300

    Finalização-front

--- Codigo Aplicativo Escolar/front-aplicativo/src/views/MenuPrincipal/MenuPrincipal.js
+++ Codigo Aplicativo Escolar/front-aplicativo/src/views/MenuPrincipal/MenuPrincipal.js
@@ -1,17 +1,24 @@
-import { Header, Footer} from "../../components";
+import { Header, Footer, EditButton} from "../../components";
 import './style.css'
-
+import mae from '../../image/dia-das-maes.jpg'
 
 const MenuPrincipal = () =>{
     return(
-            <div>
-                <Header/>
-                    <div className="back-web">
-
+        <div>
+            <Header/>
+                <div className="back-web">
+                    <div className="quadrado">
+                        <h1 className="journal-title">Jornal Escolar - 01/05/2023</h1>
+                        <img className="mae" src={mae} alt=" "></img>
+                        <h3 className="jornal-conteiner">Dia das mães está chegando e nós da escola fulano de tal, estamos preparando uma apresentação mega</h3>
+                        <h3 className="jornal-conteiner">especial para celebrar esse dia, não se esqueça de comparecer para deixar ainda mais especial esse dia</h3>
+                        <h3 className="jornal-conteiner-ass">Grata 'A direção'</h3>
+                        
+                        <EditButton/>
                     </div>
-                <Footer name='Grupao'/>
-            </div>
-  
+                </div>
+            <Footer name='Grupao'/>
+        </div>
   )
 }
 
--- Codigo Aplicativo Escolar/front-aplicativo/src/views/StudentDatas/StudentDatas.js
+++ Codigo Aplicativo Escolar/front-aplicativo/src/views/StudentDatas/StudentDatas.js
@@ -1,12 +1,36 @@
-import { Header, Footer} from "../../components";
+import { Header, Footer, EditButton} from "../../components";
+import './StudentDatas.css'
 
 const StudentDatas = () =>{
     return(
         <div>
             <Header/>
             <div className="back-web">
-                
-            </div> 
+                <div className="quadrado">
+                    <h1 className="data-title">Dados do Aluno</h1>
+                    <div className="information">
+                        <div className="table-info">
+                            <h2 className="title-login"> Nome completo: </h2>
+                            <input className="input-login" placeholder="Nome " type="text" required></input>
+                            <h2 className="title-login"> Possui Alergia? </h2>
+                            <input className="input-login" placeholder="Não? Sim? Quais " type="text" required></input>
+                            <h2 className="title-login"> Telefone para contato </h2>
+                            <input className="input-login" placeholder="Telefone " type="phone" required></input>
+                        </div>
+                        <div className="table-info">
+                            <h2 className="title-login"> Data de Nascimento: </h2>
+                            <input className="input-login" placeholder="Data de nascimento" type="date" required></input>
+                            <h2 className="title-login"> Genero </h2>
+                            <input className="input-login" placeholder="Gênero " type="text" required></input> 
+                            <h2 className="title-login"> Permite o uso de imagens? </h2>
+                            <input className="input-login" placeholder="Sim ou não" type="text" required></input> 
+                        </div>
+                    </div>
+                    <br/>
+                    <br/>
+                    <EditButton/>
+                </div>
+            </div>
             <Footer name='Grupao'/>
         </div>
     )
