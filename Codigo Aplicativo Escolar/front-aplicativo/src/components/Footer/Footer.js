adef63422e9677f51af1d4468788092703b09974
Lucas Cabrera <lucas.cabrera@fatec.sp.gov.br>
2023-05-29 09:10:04 -0300

    Att Footer

--- Codigo Aplicativo Escolar/front-aplicativo/src/components/Footer/Footer.js
+++ Codigo Aplicativo Escolar/front-aplicativo/src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = ({name}) =>{
         
         <div className='footer'>
             <button className='button' onClick={() =>helpers.Talk(name)}>Contato</button>
-            <h2>Equipe</h2>
+            <h2>Agende um Horário</h2>
             <h2>Sugestão</h2>
         </div>
     )
